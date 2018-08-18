9f0c20050b148f712fb3f32528c61c9919052fa7
devjohnsonGA <dseth.johnson@gmail.com>
2018-08-18 18:12:37 -0400

    captured input from text area and passed info to save alert

--- Exercise Files/Ch04/04_03/start/bulletin-board/src/Note.js
+++ Exercise Files/Ch04/04_03/start/bulletin-board/src/Note.js
@@ -26,14 +26,14 @@ class Note extends Component {
 	}
 
 	save() {
-		alert('saved!')
+		alert(this._newtext.value)
 	}
 
 	renderForm() {
 		return (
 			<div className="note">
 				<form>
-					<textarea />
+					<textarea ref={input => this._newtext = input}/>
 					<button onClick={this.save}><FaFloppyO /></button>
 				</form>
 			</div>
@@ -43,7 +43,7 @@ class Note extends Component {
 	renderDisplay() {
 		return (
 			<div className="note">
-				<p>Learn React</p>
+				<p>Learning React</p>
 				<span>
 					<button onClick={this.edit} id="edit"><FaPencil /></button>
 					<button onClick={this.remove} id="remove"><FaTrash /></button>
